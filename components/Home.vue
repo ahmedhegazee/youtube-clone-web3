317b6ab0ad7f08346482318d3408c44c85c7feb1
Ahmed Hegazy <ahmedhegazee@hotmail.com>
2022-09-05 16:38:45 +0200

    init project

---
+++ components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-screen">
+    <h1 class="text-6xl font-bold text-slate-900">Web3 YouTube Clone</h1>
+    <h3 class="text-2xl mt-8 text-slate-900">
+      Nuxt.js, TailwindCSS, Solidity, IPFS, The Graph and Polygon
+    </h3>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
